c212fd0c7f4bf1a37c978d4c5b194a0676e9be17
tingzhou <ting.zhou@dcfservice.net>
2016-01-08 10:50:01 +0800

    添加默认配置参数

--- files/js/myGrid.js
+++ files/js/myGrid.js
@@ -1,14 +1,22 @@
 (function($) {
     
     $.fn.myGrid = function(options) {
-        
-        data = new MYGrid(this, options);
-        console.log($.fn.myGrid.defaults)
-        return this;
+        if(arguments.length===0||typeof arguments[0]=== 'object'){
+            var option=arguments[0],
+                data=this.data('myGrid'),
+                options=$.extend({},$.fn.myGrid.defaults,option);
+            if(!data){
+                data=new MYGrid(this,options);
+                this.data('myGrid',data)
+            }
+            return $.extend(true,this,data)
+        }
     }
     //默认参数
     $.fn.myGrid.defaults={
-        nodatatext:'no data'
+        nodatatext:'no data',
+        data:[],
+        columns:[]
     }
     var MYGrid=function(element,options){
         this.opts=options;
@@ -18,9 +26,6 @@
     $.fn.myGrid.constructor=MYGrid;
     //私有方法
     MYGrid.prototype={
-        _test:function(){
-             console.log(2)
-        },
         _tableLayout:function($ele){
             var opts=this.opts;
             var thead=ths=tbody='',rows=[];
@@ -28,7 +33,7 @@
                 ths+='<th>'+opts.columns[i].datafield+'</th>';
             };
             thead='<thead><tr>'+ths+'</tr></thead>';
-            if(opts.data){
+            if(opts.data.length){
                 for(var i=0; i<opts.data.length;i++){
                     var item = opts.data[i];
                     rows.push('<tr>');
@@ -38,14 +43,22 @@
                     rows.push('</tr>');
 
                 }
-            }
-            var html='<table class="table table-hover">\
+                var html='<table class="table table-hover">\
                           '+thead+'\
                           <tbody>\
                             '+rows.join('')+'\
                           </tbody>\
                         </table>'
-            $ele.html(html)
+                $ele.html(html)
+            }else{
+                var html='<table class="table table-hover">\
+                          '+thead+'\
+                          <tbody>\
+                          </tbody>\
+                        </table><div>'+opts.nodatatext+'</div>'
+                $ele.html(html)
+            }
+            
             return this;
         }
     }
